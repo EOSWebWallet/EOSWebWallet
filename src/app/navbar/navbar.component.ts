7960c6d8f958788662ff9c701632f0f43533c0a8
andrey1894 <lebedev.andrey1894@gmail.com>
2018-10-09 10:52:49 +0300

    Feat: Logined on change network (Private key)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,8 +5,8 @@ import { LocalStorage } from 'ngx-webstorage'
 import { Router } from '@angular/router'
 import { TranslateService } from '@ngx-translate/core'
 
-import { AddEditNetworkDialogComponent, ChangeLastNetworkDialogComponent } from '../dialogs'
-import { ScatterService, LoginService, ConfigService } from '../services/'
+import { AddEditNetworkDialogComponent, ChangeLastNetworkDialogComponent, SelectAccountDialogComponent } from '../dialogs'
+import { ScatterService, LoginService, ConfigService, CryptoService, AccountService } from '../services/'
 import { LoginState } from '../models/login-state.model'
 import { Network, NetworkProtocol, NetworkChaindId } from '../models/network.model'
 
@@ -80,7 +80,9 @@ export class NavbarComponent {
     iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer,
     private translate: TranslateService,
-    public loginService: LoginService
+    public loginService: LoginService,
+    private accountService: AccountService,
+    private cryptoService: CryptoService
   ) {
     this.toggleNav = false
 
@@ -233,11 +235,85 @@ export class NavbarComponent {
         })
       }
     } else if (this.isLoggedIn === LoginState.publicKey) {
-      this.logout()
+      let isLogin = await this.loginKey()
+      if (isLogin) {
+        this.lastIdNetwork = this.selectedIdNetwork
+      } else {
+        if (this.lastIdNetwork === null) {
+          this.logout()
+        }
+        let dialogRef = this.dialog.open(ChangeLastNetworkDialogComponent)
+        await dialogRef.afterClosed().subscribe(async result => {
+          if (result) {
+            this.selectedNetwork = this.networks[this.lastIdNetwork].host
+            this.setNetwork(this.lastIdNetwork)
+            if (this.loginKey()) {
+              this.lastIdNetwork = this.selectedIdNetwork
+            } else {
+              this.lastIdNetwork = null
+              this.logout()
+            }
+          } else {
+            this.lastIdNetwork = null
+            this.logout()
+          }
+        })
+      }
+
     } else if (this.isLoggedIn === LoginState.out) {
       this.logout()
     }
+  }
 
+  async loginKey () {
+    let pubKey = this.cryptoService.decrypt(this.loginService.publicKey, this.loginService.pass)
+    let data = await this.accountService.findByKey('{"public_key":"' + pubKey + '"}').toPromise()
+
+    if (!data || !data.account_names.length) {
+      return false
+    }
+    let accountNotFoundMesage = this.translate.get('dialogs.account-not-found').toPromise()
+
+    let success = false
+    let callback = (accountName = '', permission = ''): void => {
+      if (accountName && permission) {
+        this.accountName = accountName
+        this.loginService.permission = permission
+        success = true
+      }
+    }
+
+    await this.selectPermission(data, callback)
+    return success
+  }
+
+  async selectPermission (data, callback: (accountName, permission) => any) {
+    if (data == null) {
+      callback('', '')
+      return
+    }
+    let self = this
+    let accounts = []
+    for (const account of data.account_names) {
+      let permissions = await self.accountService.findByName('{"account_name":"' + account + '"}').toPromise()
+      if (permissions) {
+        for (const item of permissions.permissions) {
+          accounts.push([account.toString(),item.perm_name])
+        }
+      }
+    }
+    if (accounts.length === 0) {
+      callback('', '')
+      return
+    }
+    const dialogConfig = new MatDialogConfig()
+    dialogConfig.closeOnNavigation = false
+    dialogConfig.disableClose = true
+    dialogConfig.data = { accounts: accounts }
+
+    let dialogRef = this.dialog.open(SelectAccountDialogComponent, dialogConfig)
+    const result = await dialogRef.afterClosed().toPromise()
+    callback(result.data.split(',')[0], result.data.split(',')[1])
   }
 
   async loginScatter (forgetIdentity = true) {
